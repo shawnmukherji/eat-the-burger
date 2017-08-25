47f71d66e6c6737badd2c6de54ec1ae9a32ce6a9
Shawn Mukherji <Shawn@Shawns-Mac.local>
2017-08-25 13:17:33 -0500

    initial updates

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers(
+
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+burger_name VARCHAR(100),
+eaten BOOLEAN,
+date TIMESTAMP);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Dummy data to seed into database--
+
+INSERT INTO burgers(burger_name, eaten, date)
+VALUES("Royale with Cheese", true, "2017-08-25 11:23:00"),
